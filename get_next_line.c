74e840580f5e17293bb13225f1ded31e6e4ebaea
정윤주 <jung-yunjoo@jeong-yunjuui-MacBookPro.local>
2021-02-02 17:46:06 +0900

    3

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,45 @@
 /*   By: yujung <yujung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 22:10:08 by yujung            #+#    #+#             */
-/*   Updated: 2021/02/02 17:22:27 by yujung           ###   ########.fr       */
+/*   Updated: 2021/02/02 17:45:00 by yujung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void free_buf(char **p, char *temp)
+static void		free_buf(char **p)
 {
-	if (p != NULL)
+	if (p != NULL && *p != NULL)
 	{
 		free(*p);
-		*p = temp;
+		*p = NULL;
 	}
 }
 
-static void				rt_line(char **line, char **buf)
+static void		rt_line(char **line, char **buf)
 {
 	int			i;
 	char		*temp;
 
 	i = 0;
+	if (*buf == NULL)
+		return ;
 	while ((*buf)[i] != '\n' && (*buf)[i] != '\0')
 		i++;
 	if ((*buf)[i] == '\n')
 	{
 		*line = ft_substr(*buf, 0, i);
 		temp = ft_strdup(&((*buf)[i + 1]));
-		free_buf(buf, temp);
+		free(*buf);
+		*buf = temp;
 		if ((*buf)[0] == '\0')
-			free_buf(buf, NULL);
+			free_buf(buf);
 	}
 	else
 	{
 		*line = ft_strdup(*buf);
-		free_buf(buf, NULL);
+		free_buf(buf);
 	}
-}
-
-int return_val(int rd, int fd, char **buf)
-{
-	if (rd < 0)
-		return (-1);
-	else if (rd == 0 && buf[fd] == NULL)
-		return (0);
-	else
-		return (1);
 }
 
 int				get_next_line(int fd, char **line)
@@ -72,5 +65,10 @@ int				get_next_line(int fd, char **line)
 			break ;
 	}
 	rt_line(line, &buf[fd]);
-	return (return_val(rd, fd, buf));
+	if (rd < 0)
+		return (-1);
+	else if (rd == 0 && buf[fd] == NULL)
+		return (0);
+	else
+		return (1);
 }
